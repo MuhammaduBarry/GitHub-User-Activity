720c6111feb6ee035150e94d4ef0cac6e1bd3301
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-10-28 11:10:51 -0400

    updating api

--- src/api.py
+++ src/api.py
@@ -1,12 +1,14 @@
-import json
 import requests
 
 def fetch_data(username) -> None:
     url = f"https://api.github.com/users/{username}/events"
     response = requests.get(url)
     if response.status_code == 200:
-        github_event_data = response.json() # -> Returns github information in json format, which is also a dict in python
+        github_event_data = response.json()  # Returns GitHub information in JSON format, which is also a dict in Python
         for event in github_event_data:
-            print(json.dumps(event["repo"], indent=4))
+            if username in event["repo"]["name"]:
+                print(event["repo"])
+                break # -> Since we only need the first value we break the loop after one run
     else:
         print(f"Failed to retrieve data: {response.status_code}")
+
